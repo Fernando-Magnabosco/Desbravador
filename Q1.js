68e2f3dda24483f9bd4f842d8393612e66a475f4
Fernando-Magnabosco <85846314+Fernando-Magnabosco@users.noreply.github.com>
2022-04-26 19:23:30 -0300

    Q1 added;

---
+++ Q1.js
@@ -0,0 +1,32 @@
+
+function init() {
+
+    var precos = [99, 139, 189]; // Preco de cada tipo de quarto
+    var taxa = 0.08;  // Taxa = 8%;
+
+    var selectedRoom = document.getElementById("tipos-quarto");
+    var qtdDiarias = document.getElementById("qtd-diarias");
+    var submit = document.getElementById("submit");
+
+    submit.addEventListener("click", function (event) {
+
+        event.preventDefault();
+
+        var qtd = qtdDiarias.value;
+        var tipo = selectedRoom.value;
+
+        var total = precos[parseInt(tipo)] * qtd * (1 + taxa);
+
+        alert("O total a pagar é de R$" + total);
+
+
+    });
+
+
+
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", init);
